d8b536f262cd66e7bddc12a4103f51fa74a334d5
Deepanshu Shah <112880677+deepanshushah2002@users.noreply.github.com>
2023-05-10 21:51:58 +0530

    Update reduce_inner_patten.java

--- reduce_inner_patten.java
+++ reduce_inner_patten.java
@@ -6,25 +6,25 @@ public class reduce_inner_patten {
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-            System.out.print(n - j);
+            System.out.print(n - j + " ");
             }
             for (int l = 0; l < 2 * (n - i) - 1; l++) {
-            System.out.print(n - i);
+            System.out.print(n - i + " ");
             }
-            for (int k = n-i+1; k < n+1; k++) {
-            System.out.print(k);
+            for (int k = n-i+1; k < n + 1; k++) {
+            System.out.print(k + " ");
             }
             System.out.println();
         }
         for (int i = 1; i < n; i++) {
-            for (int m = n; m >= i+2; m--) {
-            System.out.print(m);
+            for (int m = n; m >= i + 2; m--) {
+            System.out.print(m + " ");
             }
             for (int p = 0; p < 2 * i + 1 ; p++) {
-                System.out.print(i+1);
+                System.out.print(i + 1 + " ");
             }
             for (int o = 2 + i; o <= n; o++) {
-            System.out.print(o);
+            System.out.print(o + " ");
             }
             System.out.println();
         }
